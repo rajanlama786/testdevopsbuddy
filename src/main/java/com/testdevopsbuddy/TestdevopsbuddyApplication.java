5bae6a529babe3df5edee59cc5e27fc46068fd5f
Rajan Lama <rajan.lama786@gmail.com>
2020-09-02 08:58:10 +0545

    initial commit

---
+++ src/main/java/com/testdevopsbuddy/TestdevopsbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.testdevopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdevopsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdevopsbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testdevopsbuddy/TestdevopsbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdevopsbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdevopsbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
